9bdee4a07e40cd33b29f93d088bb1cc33c95d2e4
Mishkovec <neilsa@mail.ru>
2019-08-03 22:49:35 +0300

    feat: 1360 and 1000 design

---
+++ index.js
@@ -0,0 +1,6 @@
+let form = document.getElementById('form');
+let btn = document.getElementById('btn');
+btn.onclick = function(event) {
+    event.preventDefault();
+    form.innerHTML=`<h3>Спасибо за заявку</h3> <p style="font-size:0.8em;"> В течение часа с вами свяжется наш специалист</p>`
+}
