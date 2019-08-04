1aa09685844ba9dd66bdf120401c51815de51870
Mishkovec <neilsa@mail.ru>
2019-08-04 22:16:57 +0300

    feat: all adaptive design FF and Chrome

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let form = document.getElementById('form');
+let form = document.getElementById('form-container');
 let btn = document.getElementById('btn');
 btn.onclick = function(event) {
     event.preventDefault();
